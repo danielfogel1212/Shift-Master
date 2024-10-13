161d10149a792cde25bd04cdf1440a65687b8fdb
danielfogel1212 <162919004+danielfogel1212@users.noreply.github.com>
2024-10-13 19:27:17 +0300

    Delete Shifts/todo_backend directory

--- Shifts/todo_backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-     
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:@localhost/todo')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', "Write here your secret key")
- 
