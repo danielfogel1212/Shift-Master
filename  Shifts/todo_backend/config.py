ec5027a0d5e5439c503674019272064dd9250173
danielfogel1212 <162919004+danielfogel1212@users.noreply.github.com>
2024-10-13 19:36:14 +0300

    Add files via upload

---
+++  Shifts/todo_backend/config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+     
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:@localhost/todo')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', "Put here your secret key")
+ 
+
